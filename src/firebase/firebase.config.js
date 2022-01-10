f07dd8e6a40f4977b044a03abd94ff5b198b8ba8
saddam-satria <ardhisaddam@gmail.com>
2022-01-10 08:03:50 +0700

    modify variabel export

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,4 +1,4 @@
-export default {
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_DOMAIN,
   projectId: process.env.REACT_APP_PROJECTID,
@@ -6,3 +6,5 @@ export default {
   messagingSenderId: process.env.REACT_APP_SENDER,
   appId: process.env.REACT_APP_APPID,
 };
+
+export default firebaseConfig;

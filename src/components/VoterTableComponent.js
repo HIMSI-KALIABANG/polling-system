be06dbca90ecd4294d901ec1a5eb7162aea22752
saddam-satria <ardhisaddam@gmail.com>
2022-01-13 17:41:03 +0700

    modify background color table

--- src/components/VoterTableComponent.js
+++ src/components/VoterTableComponent.js
@@ -6,28 +6,34 @@ const VoterTableComponent = ({ voter }) => {
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
+                <thead className="bg-gray-800">
                   <tr>
-                    <th scope="col" className="font-poppins px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" className="font-poppins px-6 py-3 text-left text-xs font-medium text-yellow-500 uppercase tracking-wider">
                       Nama Pemilih
                     </th>
-                    <th scope="col" className="font-poppins px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" className="font-poppins px-6 py-3 text-left text-xs font-medium text-yellow-500 uppercase tracking-wider">
                       Kandidat yang dipilih
+                    </th>
+                    <th scope="col" className="font-poppins px-6 py-3 text-left text-xs font-medium text-yellow-500 uppercase tracking-wider">
+                      Token
                     </th>
                   </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
+                <tbody className=" divide-y divide-gray-200 bg-gray-800">
                   {voter &&
-                    voter.map(({ name, candidate }, index) => {
-                      return (
-                        <>
-                          <tr key={index}>
-                            <td className="px-6 py-4 whitespace-nowrap capitalize font-poppins">{name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap capitalize font-poppins">{candidate}</td>
-                          </tr>
-                        </>
-                      );
-                    })}
+                    voter
+                      .filter((vote) => vote.status === true)
+                      .map(({ name, candidate, token }, index) => {
+                        return (
+                          <>
+                            <tr key={index}>
+                              <td className="px-6 py-4 whitespace-nowrap capitalize font-poppins text-yellow-500">{name}</td>
+                              <td className="px-6 py-4 whitespace-nowrap capitalize font-poppins text-yellow-500">{candidate}</td>
+                              <td className="px-6 py-4 whitespace-nowrap capitalize font-poppins text-yellow-500">{token}</td>
+                            </tr>
+                          </>
+                        );
+                      })}
                 </tbody>
               </table>
             </div>

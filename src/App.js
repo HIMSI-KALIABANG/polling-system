5b8be7cdae4c1cb71c7b97a20b457fdfb54f7922
saddam-satria <ardhisaddam@gmail.com>
2022-01-13 17:43:12 +0700

    modify routing add admin path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { useCallback, useRef } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages';
+import Admin from './pages/admin';
 import Vote from './pages/vote';
 
 const App = () => {
-  const currUsers = JSON.parse(window.localStorage.getItem('users'));
-
   return (
     <>
       <Router>
         <Routes>
           <Route path={'/'} element={<Home />} />
-          <Route path={'/vote'} element={currUsers === null ? <Navigate replace={true} to={'/'} state={'Oops, anda belum bisa voting'} /> : <Vote />} />
+          <Route path={'/vote'} element={<Vote />} />
+          <Route path={'/admin'} element={<Admin />} />
         </Routes>
       </Router>
     </>

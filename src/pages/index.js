916e77ccce321fb87413d4d2ab92fe174039e281
saddam-satria <ardhisaddam@gmail.com>
2022-01-13 17:42:13 +0700

    add auth user

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,48 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import GetVoter from '../hooks/getVoter';
 
-const Home = (state) => {
-  const [name, setName] = useState('');
-  const [ipAddress, setipAddress] = useState('');
+const Home = () => {
+  const [token, setToken] = useState('');
+  const [invalidToken, setInvalidToken] = useState(false);
+  const [rules, setRules] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getIP = ({ ip }) => {
-      setipAddress(ip);
-    };
+  const { voter } = GetVoter();
 
-    fetch(`https://api.ipify.org?format=json`)
-      .then((response) => response.json())
-      .then((result) => getIP(result))
-      .catch((err) => console.log(err));
-  }, []);
+  useEffect(() => {
+    if (token === '') {
+      setRules(true);
+    } else {
+      setRules(false);
+    }
+  }, [token]);
 
   const loginHandler = (e) => {
     e.preventDefault();
 
-    if (name === '') {
+    if (token === '') {
       return navigate('/', {
         replace: true,
       });
     }
 
-    localStorage.setItem('users', JSON.stringify({ name: e.target.nama.value, ip: ipAddress }));
+    localStorage.removeItem('admin');
 
-    navigate('/vote', {
-      replace: true,
-    });
+    if (token === process.env.REACT_APP_SECRET_KEY) {
+      localStorage.setItem('admin', JSON.stringify({ secret_key: process.env.REACT_APP_SECRET_KEY }));
+      return navigate('/admin');
+    }
+
+    const validToken = voter.filter((vote) => vote.token == token);
+
+    if (validToken.length > 0) {
+      localStorage.removeItem('token');
+      localStorage.setItem('token', JSON.stringify({ token: validToken[0].token }));
+      return navigate('/vote');
+    }
+
+    setInvalidToken(true);
   };
   return (
     <>
@@ -40,10 +52,16 @@ const Home = (state) => {
       <div className="lg:mx-32 mx-8 ">
         <div className="flex items-center flex-col ">
           <form onSubmit={loginHandler}>
-            <input value={name} name={'nama'} onChange={(e) => setName(e.target.value)} type="text" placeholder="Nama Kamu" className="px-4 py-2 w-72 focus:outline-none border-1 border rounded-md shadow-md border-gray-100" />
-            <button className="py-2 px-4 ml-0 md:ml-2 md:mb-0 mt-2 bg-gray-100 shadow-md rounded-lg capitalize">Lanjut voting</button>
+            <input value={token} name={'nama'} onChange={(e) => setToken(e.target.value)} type="text" placeholder="Token" className="px-4 py-2 w-72 focus:outline-none border-1 border rounded-md shadow-md border-gray-100" />
+            {rules ? (
+              <button disabled className="py-2 px-4 ml-0 md:ml-2 md:mb-0 mt-2 bg-gray-100 shadow-md rounded-lg capitalize">
+                Token tidak boleh kosong
+              </button>
+            ) : (
+              <button className="py-2 px-4 ml-0 md:ml-2 md:mb-0 mt-2 bg-gray-100 shadow-md rounded-lg capitalize">Lanjut voting</button>
+            )}
           </form>
-          <span className="text-sm text-gray-500 font-poppins py-2">Your IP Address : {ipAddress}</span>
+          <span className="text-sm text-gray-500 font-poppins py-2">{invalidToken ? 'Token Salah' : ''}</span>
         </div>
       </div>
     </>
--- src/pages/vote.js
+++ src/pages/vote.js
@@ -1,45 +1,36 @@
 import CandidatesComponent from '../components/CandidatesComponent';
 import LoadingComponent from '../components/LoadingComponent';
 import GetCandidates from '../hooks/getCandidates';
-import GetVoter from '../hooks/getVoter';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 
 const Vote = () => {
   const candidates = GetCandidates();
-  const voter = GetVoter();
   const [trial, setTrial] = useState(false);
-
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (voter.voter) {
-      const IPList = [];
-      voter.voter.map((vote) => IPList.push(vote.ipaddress));
-      const userIp = JSON.parse(window.localStorage.getItem('users'));
-
-      if (!userIp) {
-        navigate('/');
-      }
-
-      if (IPList.includes(userIp.ip)) {
-        setTrial(true);
-      }
-    }
-  }, [voter.voter, navigate]);
+    const currentToken = JSON.parse(localStorage.getItem('token'));
+    if (!currentToken) return navigate('/');
+  }, []);
 
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div className="md:mx-28 mx-8">
           <div className="py-8">
-            <h4 className="text-xl font-poppins">Daftar Kandidat</h4>
+            <h4 className="text-xl font-poppins mb-2">Daftar Kandidat</h4>
+            {trial && (
+              <span disabled className="px-8 mt-2 py-2 rounded-lg bg-gray-50  shadow-md text-sm hover:bg-gray-100 font-poppins">
+                Maaf token sudah digunakan
+              </span>
+            )}
           </div>
+
           {!candidates.data && <LoadingComponent />}
           {candidates.data &&
             candidates.data.map((candidate, index) => {
-              return <CandidatesComponent candidate={candidate} key={index} trial={trial} />;
+              return <CandidatesComponent candidate={candidate} key={index} trial={trial} setTrial={setTrial} />;
             })}
         </div>
       </div>

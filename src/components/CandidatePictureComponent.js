2ecb3409e582b2c56b7e0443d3efff8fcbfd843a
saddam-satria <ardhisaddam@gmail.com>
2022-01-14 10:04:57 +0700

    modify UI

--- src/components/CandidatePictureComponent.js
+++ src/components/CandidatePictureComponent.js
@@ -1,12 +1,10 @@
-import React from 'react';
-
 const CandidatePictureComponent = ({ candidate: { name, count } }) => {
   return (
     <>
       <img src={process.env.PUBLIC_URL + 'img/profile.png'} alt={name} className="w-20" />
       <div className="flex flex-col justify-center items-center w-40">
-        <span className="text-xl capitalize">{name}</span>
-        <span className="text-lg text-slate-500">{count}</span>
+        <span className="text-xl capitalize text-secondary font-bold">{name}</span>
+        <span className="text-lg font-extralight text-secondary">{count}</span>
       </div>
     </>
   );
--- src/components/CandidatesComponent.js
+++ src/components/CandidatesComponent.js
@@ -1,12 +1,11 @@
 import { doc, updateDoc } from 'firebase/firestore';
-import { useState } from 'react';
 import CandidateCollection from '../firebase/collection/Candidates';
 import VoterCollection from '../firebase/collection/Voter';
 import GetVoter from '../hooks/getVoter';
 import CandidatePictureComponent from './CandidatePictureComponent';
+import * as Gi from 'react-icons/gi';
 
-const CandidatesComponent = ({ candidate: { name, count, id }, trial, setTrial }) => {
-  const [error, setError] = useState(false);
+const CandidatesComponent = ({ candidate: { name, count, id }, setTrial, setError }) => {
   const { voter } = GetVoter();
   const voteHandler = (e, id, currentCount, name) => {
     e.preventDefault();
@@ -41,15 +40,14 @@ const CandidatesComponent = ({ candidate: { name, count, id }, trial, setTrial }
       <div className="flex items-center mb-5">
         <CandidatePictureComponent candidate={{ name, count }} />
         <div>
-          {!trial && error ? (
-            <button disabled className="px-8 py-2 rounded-lg bg-gray-50  shadow-md text-sm hover:bg-gray-100 font-poppins">
-              Invalid Token
-            </button>
-          ) : (
-            <button onClick={(e) => voteHandler(e, id, count, name)} className="px-8 py-2 rounded-lg bg-gray-50  shadow-md text-sm hover:bg-gray-100 font-poppins">
-              Vote
-            </button>
-          )}
+          <button onClick={(e) => voteHandler(e, id, count, name)} className="bg-secondary tracking-wider hover:bg-opacity-90 text-utils text-sm px-6 mt-4 py-2 rounded-lg font-semibold font-poppins uppercase">
+            <div className="flex">
+              <span>vote</span>
+              <span className="m-0.5">
+                <Gi.GiVote />
+              </span>
+            </div>
+          </button>
         </div>
       </div>
     </>

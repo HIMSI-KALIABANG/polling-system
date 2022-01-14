a3b2e590fca0b6350e6bc0734f5bd69363b184e3
saddam-satria <ardhisaddam@gmail.com>
2022-01-14 10:03:41 +0700

    modify UI

--- src/pages/vote.js
+++ src/pages/vote.js
@@ -7,31 +7,39 @@ import { useNavigate } from 'react-router-dom';
 const Vote = () => {
   const candidates = GetCandidates();
   const [trial, setTrial] = useState(false);
+  const [error, setError] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     const currentToken = JSON.parse(localStorage.getItem('token'));
     if (!currentToken) return navigate('/');
-  }, [navigate]);
+
+    if (trial) {
+      setTimeout(() => {
+        setTrial(false);
+      }, 1000);
+    }
+  }, [navigate, trial]);
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div className="md:mx-28 mx-8">
-          <div className="py-8">
-            <h4 className="text-xl font-poppins mb-2">Daftar Kandidat</h4>
-            {trial && (
-              <span disabled className="px-8 mt-2 py-2 rounded-lg bg-gray-50  shadow-md text-sm hover:bg-gray-100 font-poppins">
-                Maaf token sudah digunakan
-              </span>
-            )}
+      <div className="min-h-screen bg-primary">
+        <div className="lg:mx-32 mx-8 py-8">
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="py-8">
+              <h4 className="text-xl font-poppins mb-2 text-secondary pb-4">Daftar Kandidat</h4>
+              {!candidates.data && <LoadingComponent />}
+              {candidates.data &&
+                candidates.data.map((candidate, index) => {
+                  return <CandidatesComponent candidate={candidate} key={index} trial={trial} setTrial={setTrial} setError={setError} />;
+                })}
+              {trial && (
+                <span disabled className="px-8 mt-2 py-2 rounded-lg bg-primary border-secondary border-2 border-solid text-secondary capitalize shadow-md text-sm  font-poppins">
+                  {error ? 'invalid token' : ' Maaf token sudah digunakan'}
+                </span>
+              )}
+            </div>
           </div>
-
-          {!candidates.data && <LoadingComponent />}
-          {candidates.data &&
-            candidates.data.map((candidate, index) => {
-              return <CandidatesComponent candidate={candidate} key={index} trial={trial} setTrial={setTrial} />;
-            })}
         </div>
       </div>
     </>

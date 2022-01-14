b7389cca1c3cd948a397f246c5b22e88fc79c227
saddam-satria <ardhisaddam@gmail.com>
2022-01-14 10:01:55 +0700

    modify 404 not found

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages';
+import NotFound from './pages/404.page';
 import Admin from './pages/admin';
 import Vote from './pages/vote';
 
@@ -11,7 +12,13 @@ const App = () => {
           <Route path={'/'} element={<Home />} />
           <Route path={'/vote'} element={<Vote />} />
           <Route path={'/admin'} element={<Admin />} />
+          <Route path={'*'} element={<NotFound />} />
         </Routes>
+        <div className="fixed bottom-2 left-1/2">
+          <div className="flex">
+            <span className="text-sm text-secondary font-poppins capitalize">Copyright &#169; 2022</span>
+          </div>
+        </div>
       </Router>
     </>
   );

abb6905f11e89ea5bafe47bcff2908c74e249d45
saddam-satria <ardhisaddam@gmail.com>
2022-01-14 10:01:28 +0700

    modify colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      colors: {
+        primary: '#130f40',
+        secondary: '#ffec78',
+        utils: '#1e1633',
+        title: '#fef8fd',
+      },
     },
   },
   plugins: [],

ded183b7f8f6720dfd8bf22fd93e5fd11df509ea
saddam-satria <ardhisaddam@gmail.com>
2022-01-21 10:38:30 +0700

    modify add routing closing and remove footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FormCandidate from './components/FormCandidate';
+import GolputTableComponent from './components/GolputTableComponent';
 import VisionMissonComponent from './components/VisionMissonComponent';
 import VoterTableComponent from './components/VoterTableComponent';
 import GetVoter from './hooks/getVoter';
 import Home from './pages';
 import NotFound from './pages/404.page';
 import Admin from './pages/admin';
+import Closing from './pages/Closing';
 import Vote from './pages/vote';
 
 const App = () => {
@@ -16,20 +18,17 @@ const App = () => {
       <Router>
         <Routes>
           <Route path={'/'} element={<Home />} />
+          <Route path={'/closing'} element={<Closing />} />
           <Route path={'/vote'} element={<Vote />}>
             <Route path=":name" element={<VisionMissonComponent />} />
           </Route>
           <Route path={'/admin'} element={<Admin />}>
             <Route path={'/admin'} element={<VoterTableComponent voter={voter} />} />
             <Route path={'participant'} element={<FormCandidate />} />
+            <Route path={'golput'} element={<GolputTableComponent voter={voter} />} />
           </Route>
           <Route path={'*'} element={<NotFound />} />
         </Routes>
-        <div className="fixed bottom-2 left-1/2">
-          <div className="flex">
-            <span className="text-sm text-secondary font-poppins capitalize">Copyright &#169; 2022</span>
-          </div>
-        </div>
       </Router>
     </>
   );
